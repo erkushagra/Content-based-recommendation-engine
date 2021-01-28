04b5fe0608298307242a32a4c8127312ca1a74d8
yd09 <66961735+erkushagra@users.noreply.github.com>
2021-01-28 19:45:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import flask
+import difflib
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = flask.Flask(__name__, template_folder='templates')
 
-df2 = pd.read_csv('model/tmdb.csv')
+df2 = pd.read_csv('./model/tmdb.csv')
 
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(df2['soup'])
@@ -16,7 +17,6 @@ df2 = df2.reset_index()
 indices = pd.Series(df2.index, index=df2['title'])
 all_titles = [df2['title'][i] for i in range(len(df2['title']))]
 
-
 def get_recommendations(title):
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     idx = indices[title]
@@ -26,24 +26,24 @@ def get_recommendations(title):
     movie_indices = [i[0] for i in sim_scores]
     tit = df2['title'].iloc[movie_indices]
     dat = df2['release_date'].iloc[movie_indices]
-    return_df = pd.DataFrame(columns=['Title', 'Year'])
+    return_df = pd.DataFrame(columns=['Title','Year'])
     return_df['Title'] = tit
     return_df['Year'] = dat
     return return_df
 
-
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
+
 def main():
     if flask.request.method == 'GET':
-        return flask.render_template('index.html')
-
+        return(flask.render_template('index.html'))
+            
     if flask.request.method == 'POST':
         m_name = flask.request.form['movie_name']
         m_name = m_name.title()
-        #        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
+#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
         if m_name not in all_titles:
-            return flask.render_template('negative.html', name=m_name)
+            return(flask.render_template('negative.html',name=m_name))
         else:
             result_final = get_recommendations(m_name)
             names = []
@@ -52,9 +52,7 @@ def main():
                 names.append(result_final.iloc[i][0])
                 dates.append(result_final.iloc[i][1])
 
-            return flask.render_template('positive.html', movie_names=names, movie_date=dates, search_name=m_name)
-
+            return flask.render_template('positive.html',movie_names=names,movie_date=dates,search_name=m_name)
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
